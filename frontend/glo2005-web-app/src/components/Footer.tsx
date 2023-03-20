c6482b69afaeb2f7f1e9779951805de708be7fe4
Jérémy Doiron <jeremy.doiron.1@ulaval.ca>
2023-03-20 19:20:03 -0400

    added Footer content

--- frontend/glo2005-web-app/src/components/Footer.tsx
+++ frontend/glo2005-web-app/src/components/Footer.tsx
@@ -1,6 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <ul>
+        <li>@ 2023 Immofab, Inc.</li>
+        <li>·</li>
+        <li>
+          <Link to="/">Page d'accueil</Link>
+        </li>
+        <li>·</li>
+        <li>
+          <Link to="/about">À propos</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
 
