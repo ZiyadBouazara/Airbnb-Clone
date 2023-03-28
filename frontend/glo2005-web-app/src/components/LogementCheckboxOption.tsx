2a5b1220d3bc84e897b0d292785223d9fbc5e755
Jérémy Doiron <jeremy.doiron.1@ulaval.ca>
2023-03-28 18:08:10 -0400

    fixed LogementCheckbox display

--- frontend/glo2005-web-app/src/components/LogementCheckboxOption.tsx
+++ frontend/glo2005-web-app/src/components/LogementCheckboxOption.tsx
@@ -6,10 +6,10 @@ interface Props {
 const LogementCheckboxOption: React.FC<Props> = ({ id, option }) => {
   return (
     <li>
-        <input type="checkbox" id={id} value="" className="hidden peer"/>
-        <label htmlFor={id} className="w-full p-5 border-2 rounded-lg cursor-pointer peer-checked:border-immofab hover:bg-gray-100">
-            <span className="w-full">{ option }</span>
-        </label>
+      <input type="checkbox" id={id} value="" className="hidden peer" required/>
+      <label htmlFor={id} className="flex items-center justify-center w-full p-5 border-2 rounded-lg cursor-pointer peer-checked:border-immofab hover:bg-gray-100 font-semibold">
+        <span>{ option }</span>
+      </label>
     </li>
   )
 }
