76274fead7af8f025f21722a646aac8f3f400bc0
Jérémy Doiron <jeremy.doiron.1@ulaval.ca>
2023-03-29 00:31:13 -0400

    added login and signup modal

--- frontend/glo2005-web-app/src/components/HeaderListIcon.tsx
+++ frontend/glo2005-web-app/src/components/HeaderListIcon.tsx
@@ -1,17 +1,48 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Modal from "./Modal";
+import LogInModal from "./LoginModal";
+import SignUpModal from "./SignUpModal";
 
 interface Props {
   isLoggedIn: boolean;
 }
 
 const HeaderListIcon: React.FC<Props> = ({ isLoggedIn }) => {
-  return (
-    <li className="relative w-10 h-10 mx-2 mb-2 overflow-hidden hover:bg-gray-100 rounded-full">
-      <Link to="/account">
-        <svg className="absolute w-12 h-12 text-black -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-      </Link>
-    </li>
-  )
+  const [isOpen, setOpen] = useState(false);
+  const [isLogin, setLogin] = useState(true);
+
+  const toggleOpen: () => void = () => {
+    setOpen(!isOpen);
+  }
+
+  const toggleLogin: () => void = () => {
+    setLogin(!isLogin);
+  }
+
+  const openModal: () => void = () => {
+    setLogin(true);
+    toggleOpen();
+  }
+
+  if (isLoggedIn) {
+    return (
+      <li className="relative w-10 h-10 mx-2 mb-2 overflow-hidden hover:bg-gray-100 rounded-full">
+        <Link to="/account">
+          <svg className="absolute w-12 h-12 text-black -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+        </Link>
+      </li>
+    )
+  } else {
+    return (
+      <li className="relative w-10 h-10 mx-2 mb-2 overflow-hidden hover:bg-gray-100 rounded-full cursor-pointer">
+        <svg className="absolute w-12 h-12 text-black -left-1" onClick={openModal} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd"></path></svg>
+        <Modal isOpen={isOpen} toggleOpen={toggleOpen}>
+          {isLogin ? <LogInModal toggleLogin={toggleLogin} /> : <SignUpModal toggleLogin={toggleLogin}/> }
+        </Modal>
+      </li>
+    )
+  }
 }
 
 export default HeaderListIcon
---
+++ frontend/glo2005-web-app/src/components/LoginModal.tsx
@@ -0,0 +1,27 @@
+interface Props {
+    toggleLogin: () => void;
+}
+
+const LogInModal: React.FC<Props> = ({ toggleLogin }) => {
+  return (
+    <div className="px-6 py-6 lg:px-8">
+        <h3 className="mb-4 text-xl font-medium">Connexion</h3>
+        <form className="space-y-6" action="#">
+            <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium">Courriel</label>
+                <input type="email" name="email" id="email" className="border border-gray-200 text-sm rounded-lg block w-full p-2.5" placeholder="Courriel" required />
+            </div>
+            <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium">Mot de passe</label>
+                <input type="password" name="password" id="password" placeholder="••••••••" className="border border-gray-200 text-sm rounded-lg block w-full p-2.5" required />
+            </div>
+            <button type="submit" className="w-full text-white bg-immofab hover:bg-immofab-hover font-medium rounded-lg text-sm px-5 py-2.5 text-center">Connection</button>
+            <div className="text-sm font-medium">
+                Pas de compte? <span className="text-blue-700 hover:underline visited:text-purple-600 cursor-pointer" onClick={toggleLogin}>Inscription</span>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default LogInModal
