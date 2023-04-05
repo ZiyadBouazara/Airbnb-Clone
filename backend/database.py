3309ba0ff043adac463ce332d23115c0d2768119
sebGerv <seger18@ulaval.ca>
2023-04-05 13:47:01 -0400

    Added backend for getFavorites, getImmeuble and getImmeubles APIs

--- backend/database.py
+++ backend/database.py
@@ -26,6 +26,23 @@ def check_user_mdp(email, mdp):
     hashed_mdp = cursor.fetchone()[0]
     return pbkdf2_sha256.verify(mdp, hashed_mdp)
 
+def get_user_favorites(userId):
+    # Cette fonction retourne les tuples des logements favoris d'un utilisateur
+    sqlRequest = f"SELECT * FROM Logement AS l INNER JOIN Aime AS a ON a.id_logement = l.id_logement WHERE id = '{userId}';"
+    cursor.execute(sqlRequest)
+    logements_favoris = cursor.fetchone()[0]
+    return logements_favoris
+
+def get_immeubles(immeubleAdresse=None):
+    # Cette fonction retourne un ou plusieurs immeubles
+    if immeubleAdresse is not None:
+        sqlRequest = f"SELECT * FROM Immeuble WHERE address = '{immeubleAdresse}';"
+    else:
+        sqlRequest = "SELECT * FROM Immeuble;"
+    cursor.execute(sqlRequest)
+    immeubles = cursor.fetchone()[0]
+    return immeubles
+
 
 
 if __name__ == '__main__':
--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from database import insert_user, check_user_mdp
+from database import insert_user, check_user_mdp, get_user_favorites, get_immeubles
 
 app = Flask(__name__)
 
@@ -8,15 +8,14 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["POST"])
 def login():
     # Valide le mot de passe d'un utilisateur
     # Retourne : status : 200 pour un mot de passe valide
+    #               ou
     #            status : 403 pour un mot de passe non valide
 
-    if request.method == "GET":
-        return render_template("index.html") # TODO retourner le bon template pour le login
-    else:
+    if request.method == "POST":
 
         data = request.json
 
@@ -36,10 +35,34 @@ def login():
         return jsonify(response)
 
 
+@app.route("/users/<user_id>/favorites", methods=["GET"])
+def getFavorites(user_id):
+    # Fonction qui retourne les logements favoris d'un utilisateur
+    # Retourne status : 200 pour un succès
+    #          favoris : tuples des logements favoris
+    #               ou
+    #          status : 204 pour un succès, mais l'utilisateur n'a pas de logements favoris
+    #          favoris : tuples des logements favoris (vide)
+
+    if request.method == "GET":
+
+        logements_favoris = get_user_favorites(user_id)
+        if logements_favoris:
+            response = {
+                "status": 200,
+                "favoris": f"{logements_favoris}"
+            }
+        else:
+            response = {
+                "status": 204,
+                "favoris": f"{logements_favoris}"
+            }
+        return jsonify(response)
+
 @app.route("/signin", methods=["POST"])
 def signin():
     # Insère un nouvel utilisateur dans la base de données
-    # Retourne status : 200 pour un succès
+    # Retourne status : 201 pour un succès
     if request.method == "POST":
 
         data = request.json
@@ -53,12 +76,54 @@ def signin():
         insert_user(email, phone, nom, mdp, age)
 
         response = {
-            "status": 200
+            "status": 201
         }
 
         return jsonify(response)
 
+@app.route("/immeubles", methods=["GET"])
+def getImmeubles():
+    # Fonction qui retourne tous les immeubles
+    # Retourne status : 200 pour un succès
+    #          immeubles : tuples des immeubles
+    #               ou
+    #          status : 204 pour un succès, mais il n'y a pas d'immeubles
+    #          immeubles : tuples des immeubles (vide)
+    if request.method == "GET":
+        immeubles = get_immeubles()
+        if immeubles:
+            response = {
+                "status": 200,
+                "immeubles": f"{immeubles}"
+            }
+        else:
+            response = {
+                "status": 204,
+                "immeubles": f"{immeubles}"
+            }
+        return jsonify(response)
 
+@app.route("/immeubles/<immeuble_adresse>", methods=["GET"])
+def getImmeubles(immeuble_adresse):
+    # Fonction qui retourne le tuple d'un immeuble en fonction de son adresse
+    # Retourne status : 200 pour un succès
+    #          immeuble : tuples de l'immeuble
+    #               ou
+    #          status : 204 pour un succès, mais il n'y a pas d'immeuble a cette adresse
+    #          immeuble : tuples de l'immeuble (vide)
+    if request.method == "GET":
+        immeuble = get_immeubles(immeuble_adresse)
+        if immeuble:
+            response = {
+                "status": 200,
+                "immeuble": f"{immeuble}"
+            }
+        else:
+            response = {
+                "status": 204,
+                "immeuble": f"{immeuble}"
+            }
+        return jsonify(response)
 
 
 if __name__ == '__main__':
