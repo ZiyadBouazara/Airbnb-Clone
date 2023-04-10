4e023d1101ee744a0f57391879a98aec545de60b
Jérémy Doiron <jeremy.doiron.1@ulaval.ca>
2023-04-10 00:16:21 -0400

    Updated carousel

--- frontend/glo2005-web-app/src/components/carousel/Carousel.tsx
+++ frontend/glo2005-web-app/src/components/carousel/Carousel.tsx
@@ -25,16 +25,18 @@ const Carousel: React.FC<Props> = ({ photos, to }) => {
                 (currentIndex === index) ? <CarouselItem key={index} photo={photo} to={to} /> : null
             ))}
         </div>
-        <div className="carousel-btn absolute left-0 m-2">
-            <button className="text-sm font-semibold bg-black text-white py-2 px-4 rounded-full opacity-70 hover:opacity-100" onClick={previous}>
-            &lt;
-            </button>
-        </div>
-        <div className="carousel-btn absolute right-0 m-2">
-            <button className="text-sm font-semibold bg-black text-white py-2 px-4 rounded-full opacity-70 hover:opacity-100" onClick={next}>
-            &gt;
-            </button>
-        </div>
+        <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-2 cursor-pointer group focus:outline-none" onClick={previous}>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none">
+            <svg aria-hidden="true" className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path></svg>
+            <span className="sr-only">Previous</span>
+        </span>
+    </button>
+    <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-2 cursor-pointer group focus:outline-none" onClick={next}>
+        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 group-hover:bg-white/50 group-focus:outline-none">
+            <svg aria-hidden="true" className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
+            <span className="sr-only">Next</span>
+        </span>
+    </button>
     </div>
   )
 }
